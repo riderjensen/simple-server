c12699cda282896eceebdf45ef5a5bb5535aeda3
Rider <riderjensen@gmail.com>
2019-01-16 15:33:04 -0700

    Changing app to express

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const http = require('http');
+const express = require('express');
 
-const ourServer = http.createServer((req, res) => {
-	if (req.url == '/api') {
-		res.setHeader('Content-Type', 'application/json');
-		res.write(JSON.stringify({
-			oneItem: 'here is the first item',
-			twoItem: 'here is the second item',
-			threeItem: 'here is the third item'
-		}));
-		return res.end();
-	} else if (req.url == '/') {
-		res.setHeader('Content-Type', 'text/html');
-		res.write('<!DOCTYPE html> <html lang = "en" > ')
-		res.write('<head></head><body>');
-		res.write('<h1>Welcome to my simple server</h1>');
-		res.write('</body></html>')
-		return res.end();
-	} else {
-		res.setHeader('Content-Type', 'text/html');
-		res.write('<!DOCTYPE html> <html lang = "en" > ')
-		res.write('<head></head><body>');
-		res.write('<h1>404</h1>');
-		res.write('</body></html>')
-	}
+const app = express();
+const apiRoute = require('./routes/api');
+
+app.use('/api', apiRoute)
+
+app.get('/', (req, res, next) => {
+	res.send('<h1>HULLO??? Get the heckin heck off my server!</h1');
 });
 
-ourServer.listen(5000, () => console.log('run it'));
+
+app.listen(5000);
