8211938a34ac164eab2a94feb12d764b4d7ed8b2
Rider <riderjensen@gmail.com>
2019-01-23 15:32:22 -0700

    Organizing

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const router = express.Router();
 
 const apiRouter = require('../controllers/api');
-
-const router = express.Router();
 
 router.get('/', apiRouter.sendApiData);
 
