54521de63f7897d45d789ae22f765ddc40dfb160
Rider <riderjensen@gmail.com>
2019-01-28 15:45:27 -0700

    INstruction route rename

--- routes/store.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const storeRouter = require('../controllers/store.controller');
-
-router.get('/', storeRouter.storeHome);
-
-module.exports = router;
