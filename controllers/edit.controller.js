af2fcd0096309cfa7c38ee4e883bbd004c0ec05c
Rider <riderjensen@gmail.com>
2019-01-28 15:48:49 -0700

    Edit error in place

--- controllers/edit.controller.js
+++ controllers/edit.controller.js
@@ -15,6 +15,12 @@ exports.editOne = (req, res, next) => {
 	const newTitle = req.body.title;
 	const newPrice = req.body.price;
 
+	if (newTitle == null || newPrice == null) {
+		return res.status(500).send({
+			message: 'Please check your data and make sure you are sending title and price in the body'
+		});
+	}
+
 	itemModel.findOneAndUpdate(theId, {
 		name: newTitle,
 		price: newPrice
