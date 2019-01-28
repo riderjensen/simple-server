7e970f091bf96f7f6a9bf95ee8f9d6101b2d0fd4
Rider <riderjensen@gmail.com>
2019-01-28 15:25:42 -0700

    add edit controller

---
+++ controllers/edit.controller.js
@@ -0,0 +1,51 @@
+const itemModel = require('../models/item.model');
+
+exports.doEdit = (req, res, next) => {
+
+	// show all products with an edit button
+	itemModel.find().then(response => {
+		res.send(response);
+	})
+}
+
+exports.editOne = (req, res, next) => {
+	// get the id and edit the information
+	const theId = req.params.id;
+
+	const newTitle = req.body.title;
+	const newPrice = req.body.price;
+
+	itemModel.findOneAndUpdate(theId, {
+		name: newTitle,
+		price: newPrice
+	}).then(item => {
+		res.send(item);
+	})
+
+}
+
+exports.deleteOne = (req, res, next) => {
+	// get the id and delete the one
+	const theId = req.params.id;
+	itemModel.findByIdAndDelete(theId).then(response => {
+		if (!response) {
+			//error
+			return
+		}
+		res.send(response);
+	});
+
+}
+
+exports.createOne = (req, res, next) => {
+	// get the id and delete the one
+	const title = req.body.title;
+	const price = req.body.price;
+	const myNewItem = new itemModel({
+		name: title,
+		price: price
+	});
+	myNewItem.save().then(response => {
+		res.send(response);
+	});
+}
