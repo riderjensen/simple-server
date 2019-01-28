423ac85dead878c2608f2e55b7406a18a9281aba
Rider <riderjensen@gmail.com>
2019-01-28 15:47:27 -0700

    delete store controller

--- controllers/store.controller.js
+++
@@ -1,9 +0,0 @@
-const itemModel = require('../models/item.model');
-
-
-exports.storeHome = (req, res, next) => {
-	// send all items that we are selling
-	itemModel.find().then(response => {
-		res.send(response);
-	})
-}
