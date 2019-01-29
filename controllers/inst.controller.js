a613270a8575711a563be862c49633aae4654543
Rider <riderjensen@gmail.com>
2019-01-28 17:02:41 -0700

    Send a status

--- controllers/inst.controller.js
+++ controllers/inst.controller.js
@@ -1,5 +1,5 @@
 exports.instHome = (req, res, next) => {
-	res.send(`<h1>Welcome to the instruction area!</h1> <ul>
+	res.status(200).send(`<h1>Welcome to the instruction area!</h1> <ul>
 	<li> / - Gets all the products in the store</li>
 	<li> /instructions - get some basic instructions on how this api works</li>
 	<li> /edit/create/ - create a new product, requires title and price as strings to be sent in the body</li>
